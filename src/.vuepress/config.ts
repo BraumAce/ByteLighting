7168e14bc2a05f023920f26f395e9afdbf858bea
BraumAce <1693717911@qq.com>
2024-07-01 11:42:48 +0800

    init

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,26 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  dest: "./dev-ops/nginx/html",
+  base: "/ByteLighting/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Blog Demo",
+      description: "A blog demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "博客演示",
+      description: "vuepress-theme-hope 的博客演示",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
