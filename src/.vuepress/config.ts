e17316463fc174ace0af0c1ba38ee4dae743460a
BraumAce <1693717911@qq.com>
2024-07-02 20:03:02 +0800

    添加搜索栏

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,6 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
+  shouldPrefetch: undefined,
   base: "/ByteLighting/",
 
   locales: {
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -98,6 +98,14 @@ export default hopeTheme({
   plugins: {
     blog: true,
 
+    searchPro: {
+      // 索引全部内容
+      indexContent: true,
+
+      // 热更新
+      hotReload: true,
+    },
+
     // Install @waline/client before enabling it
     // Note: This is for testing ONLY!
     // You MUST generate and use your own comment service in production.
