a4d445d7afce4a225abe52f6f5c801403a6e3f41
BraumAce <1693717911@qq.com>
2024-07-07 12:02:04 +0800

    新增algorithm侧边栏

---
+++ src/.vuepress/sidebar/algorithm.ts
@@ -0,0 +1,10 @@
+import { arraySidebar } from "vuepress-theme-hope";
+
+export const algorithm = arraySidebar([
+  {
+    text: "ACM笔记",
+    icon: "book",
+    prefix: "/algorithm/",
+    children: "structure",
+  },
+]);
