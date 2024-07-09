b9b4167ff979cf8152f7f798fcd4cbbf84787b16
BraumAce <1693717911@qq.com>
2024-07-09 21:41:30 +0800

    更新algorithm侧边栏

--- src/.vuepress/sidebar/algorithm.ts
+++ src/.vuepress/sidebar/algorithm.ts
@@ -4,7 +4,19 @@ export const algorithm = arraySidebar([
   {
     text: "ACM笔记",
     icon: "book",
-    prefix: "/algorithm/",
+    prefix: "ACM/",
+    children: "structure",
+  },
+  {
+    text: "剑指offer",
+    icon: "book",
+    prefix: "for-offer/",
+    children: "structure",
+  },
+  {
+    text: "LeetCode",
+    icon: "book",
+    prefix: "leetcode/",
     children: "structure",
   },
 ]);
