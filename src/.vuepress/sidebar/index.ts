a6310cef7706505929dbc00c412231c602f3155d
BraumAce <1693717911@qq.com>
2024-07-09 21:42:12 +0800

    更新侧边栏

--- src/.vuepress/sidebar/index.ts
+++ src/.vuepress/sidebar/index.ts
@@ -1,7 +1,15 @@
 import { sidebar } from "vuepress-theme-hope";
+import {algorithm} from "./algorithm.js";
+import {information} from "./information.js";
+import {program} from "./program.js";
+import {openSourceProject} from "./open-source-project.js";
 
 export default sidebar({
   // 应该把更精准的路径放前面
+  "/information/": information,
+  "/program/": program,
+  "/algorithm/": algorithm,
+  "/open-source-project/": openSourceProject,
 
   // 必须放在最后面
   "/": [
