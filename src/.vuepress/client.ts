229b4dd4f95037f57cdbec31a50ba4ebe7246a19
BraumAce <1693717911@qq.com>
2024-07-13 17:43:57 +0800

    主题美化

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -2,12 +2,18 @@ import { defineClientConfig } from "vuepress/client";
 import { setupRunningTimeFooter } from "vuepress-theme-hope/presets/footerRunningTime.js";
 import { setupTransparentNavbar } from "vuepress-theme-hope/presets/transparentNavbar.js";
 import "vuepress-theme-hope/presets/shinning-feature-panel.scss";
-import "vuepress-theme-hope/presets/left-blog-info.scss";
-import "vuepress-theme-hope/presets/round-blogger-avatar.scss";
-import "vuepress-theme-hope/presets/bounce-icon.scss";
+import "vuepress-theme-hope/presets/left-blog-info.scss";    // 左侧信息面板
+import "vuepress-theme-hope/presets/round-blogger-avatar.scss";    // 圆形头像
+import "vuepress-theme-hope/presets/bounce-icon.scss";    // 图标悬停跳动
+import BlogBeautify from "./components/BlogBeautify.vue";
+import TopNavBeautify from "./components/TopNavBeautify.vue";
+import NavMusic from "./components/NavMusic.vue";
+import MyIcon from "./components/MyIcon.vue";
 
 export default defineClientConfig({
-  enhance() {},
+  enhance( { app } ) {
+    app.component('MyIcon', MyIcon);
+  },
   setup() {
     setupRunningTimeFooter(
       new Date("2024-06-30"),
@@ -21,5 +27,9 @@ export default defineClientConfig({
     })
   },
   layouts: {},
-  rootComponents: [],
+  rootComponents: [
+    BlogBeautify,
+    TopNavBeautify,
+    NavMusic,
+  ],
 });
---
+++ src/.vuepress/components/BlogBeautify.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="none">主题美化</div>
+</template>
+
+<style lang="scss">
+.vp-back-to-top-button {
+  // 返回按钮的透明度
+  opacity: 1;
+}
+
+.vp-project-card {
+  box-shadow: 0 1px 3px 1px var(--card-shadow);
+}
+
+//  hover 后出现 的小标签
+[aria-label][data-balloon-pos]:after {
+  border-radius: 0.8rem;
+}
+[aria-label][data-balloon-pos]:hover:before,
+[aria-label][data-balloon-pos]:hover:after,
+[aria-label][data-balloon-pos][data-balloon-visible]:before,
+[aria-label][data-balloon-pos][data-balloon-visible]:after,
+[aria-label][data-balloon-pos]:not([data-balloon-nofocus]):focus:before,
+[aria-label][data-balloon-pos]:not([data-balloon-nofocus]):focus:after {
+  opacity: 0.7;
+}
+
+</style>
