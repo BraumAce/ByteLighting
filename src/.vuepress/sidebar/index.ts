f5a10a5be27d8c753dcee4b367c6320961eb2286
BraumAce <1693717911@qq.com>
2024-07-16 21:53:30 +0800

    更新侧边栏

--- src/.vuepress/sidebar/index.ts
+++ src/.vuepress/sidebar/index.ts
@@ -1,8 +1,8 @@
 import { sidebar } from "vuepress-theme-hope";
-import {algorithm} from "./algorithm.js";
-import {information} from "./information.js";
-import {program} from "./program.js";
-import {openSourceProject} from "./open-source-project.js";
+import { algorithm } from "./algorithm.js";
+import { information } from "./information.js";
+import { program } from "./program.js";
+import { openSourceProject } from "./open-source-project.js";
 
 export default sidebar({
   // 应该把更精准的路径放前面
@@ -10,28 +10,4 @@ export default sidebar({
   "/program/": program,
   "/algorithm/": algorithm,
   "/open-source-project/": openSourceProject,
-
-  // 必须放在最后面
-  "/": [
-    "",
-    {
-      text: "如何使用",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    {
-      text: "文章",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
-    "intro",
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
-    },
-  ],
 });
