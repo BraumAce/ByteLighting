195feedfad96a23c26089f1283f7bc657b3173c6
BraumAce <1693717911@qq.com>
2024-07-17 21:50:46 +0800

    新增开发框架侧边栏

---
+++ src/.vuepress/sidebar/architecture.ts
@@ -0,0 +1,10 @@
+import { arraySidebar } from "vuepress-theme-hope";
+
+export const architecture = arraySidebar([
+  {
+    text: "Spring Cloud Alibaba",
+    icon: "",
+    prefix: "",
+    children: "structure",
+  },
+]);
