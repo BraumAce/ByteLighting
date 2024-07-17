efaef65d8d6b9d0cc9fcb6a0a9c43e6fe0544e01
BraumAce <1693717911@qq.com>
2024-07-17 21:51:14 +0800

    更新开源项目侧边栏

--- src/.vuepress/sidebar/open-source-project.ts
+++ src/.vuepress/sidebar/open-source-project.ts
@@ -2,8 +2,33 @@ import { arraySidebar } from "vuepress-theme-hope";
 
 export const openSourceProject = arraySidebar([
   {
-    text: "开源项目",
+    text: "用户中心",
+    icon: "user",
+    prefix: "",
+    children: "structure",
+  },
+  {
+    text: "用户匹配系统",
     icon: "",
+    prefix: "",
+    children: "structure",
+  },
+  {
+    text: "RPC框架",
+    icon: "",
+    prefix: "",
+    children: "structure",
+  },
+  {
+    text: "聊天室",
+    icon: "",
+    prefix: "",
+    children: "structure",
+  },
+  {
+    text: "AI智能答题",
+    icon: "",
+    prefix: "",
     children: "structure",
   },
 ]);
