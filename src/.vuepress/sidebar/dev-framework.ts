90c289bde1bca3500ef9ba73922db643b8e39032
BraumAce <1693717911@qq.com>
2024-07-19 21:51:33 +0800

    更新：开发框架

---
+++ src/.vuepress/sidebar/dev-framework.ts
@@ -0,0 +1,28 @@
+import { arraySidebar } from "vuepress-theme-hope";
+
+export const devFramework = arraySidebar([
+  {
+    text: "Spring",
+    icon: "",
+    prefix: "",
+    children: "structure",
+  },
+  {
+    text: "SpringBoot",
+    icon: "",
+    prefix: "",
+    children: "structure",
+  },
+  {
+    text: "SpringCloud",
+    icon: "",
+    prefix: "",
+    children: "structure",
+  },
+  {
+    text: "Vue",
+    icon: "",
+    prefix: "",
+    children: "structure",
+  },
+]);
