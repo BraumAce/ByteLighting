c35f9e8993e5008191076a84514af13b75261e0d
BraumAce <1693717911@qq.com>
2024-07-23 21:42:38 +0800

    update：常用框架

---
+++ src/.vuepress/sidebar/framework.ts
@@ -0,0 +1,34 @@
+import { arraySidebar } from "vuepress-theme-hope";
+
+export const framework = arraySidebar([
+  {
+    text: "Spring",
+    icon: "spring",
+    prefix: "spring/",
+    children: "structure",
+  },
+  {
+    text: "SpringBoot",
+    icon: "spring-boot",
+    prefix: "spring-boot/",
+    children: "structure",
+  },
+  {
+    text: "SpringCloud",
+    icon: "spring-cloud",
+    prefix: "spring-cloud/",
+    children: "structure",
+  },
+  {
+    text: "Vue",
+    icon: "vue",
+    prefix: "vue/",
+    children: "structure",
+  },
+  {
+    text: "RuoYi",
+    icon: "",
+    prefix: "ruoyi/",
+    children: "structure",
+  },
+]);
