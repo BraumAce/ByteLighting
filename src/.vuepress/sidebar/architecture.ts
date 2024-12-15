5375455d0f2eb655870b49b6fc4601314632520c
BraumAce <1693717911@qq.com>
2024-12-16 00:40:26 +0800

    更新架构设计侧边栏

--- src/.vuepress/sidebar/architecture.ts
+++ src/.vuepress/sidebar/architecture.ts
@@ -3,9 +3,38 @@ import { arraySidebar } from "vuepress-theme-hope";
 export const architecture = arraySidebar([
   {
     text: "系统设计",
-    icon: "",
+    icon: "design",
     prefix: "system-design/",
-    children: "structure",
+    children: [
+        {
+            text: "架构模式",
+            icon: "pattern",
+            prefix: "pattern/",
+            collapsible: true,
+            children: "structure",
+        },
+        {
+            text: "认证鉴权",
+            icon: "security",
+            prefix: "security/",
+            collapsible: true,
+            children: "structure",
+        },
+        {
+            text: "评论系统",
+            icon: "comment",
+            prefix: "comment/",
+            collapsible: true,
+            children: "structure",
+        },
+        {
+            text: "点赞系统",
+            icon: "good",
+            prefix: "upvote/",
+            collapsible: true,
+            children: "structure",
+        },
+    ],
   },
   {
     text: "Spring Cloud Alibaba",
