98fdfd638b730c05083d7110417d168917cbf34b
BraumAce <1693717911@qq.com>
2024-12-16 00:39:27 +0800

    更新侧边栏

--- src/.vuepress/sidebar/index.ts
+++ src/.vuepress/sidebar/index.ts
@@ -1,7 +1,7 @@
 import { sidebar } from "vuepress-theme-hope";
 import { algorithm } from "./algorithm.js";
 import { program } from "./program.js";
-import { openSourceProject } from "./open-source-project.js";
+import { openProject } from "./open-project.js";
 import { framework } from "./framework.js";
 import { architecture } from "./architecture.js";
 import { computer } from "./computer.js";
@@ -10,7 +10,7 @@ export default sidebar({
   // 应该把更精准的路径放前面
   "/program/": program,
   "/algorithm/": algorithm,
-  "/open-source-project/": openSourceProject,
+  "/open-project/": openProject,
   "/computer/": computer,
   "/framework/": framework,
   "/architecture/": architecture,
